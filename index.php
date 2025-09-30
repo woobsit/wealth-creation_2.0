9326a4ffe196281f95f02a32696eeff5b179b2c4
opetoks <opetoks@gmail.com>
2025-09-30 14:51:05 +0100

    removed wc-lts git

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	ob_start();
 	ini_set('session.cookie_lifetime', 60 * 60 * 24);
 	ini_set('session.gc-maxlifetime', 60 * 60 * 24);
-	session_set_cookie_params(86400);
+	session_set_cookie_params(86400);              
 	session_start();
 	require_once 'include/dbconfig.php';
 
